20f38f9b98eec16e4e77f0e3d151601f999a7c92
Zach Liibbe <zliibbe@gmail.com>
2022-04-20 14:03:27 -0600

    Add Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -9,30 +9,22 @@ class Form extends React.Component {
 
   render () {
     return (
-      <form className="weekly-form">
-        <label htmlFor="monday-checkbox">Monday</label>
-        <input id="monday" name="monday-checkbox" value="Monday" type="checkbox"></input>
-        
-        <label htmlFor="tuesday-checkbox">Tuesday</label>
-        <input id="tuesday" name="tuesday-checkbox" value="Tuesday" type="checkbox"></input>
-
-        <label htmlFor="wednesday-checkbox">Wednesday</label>
-        <input id="wednesday" name="wednesday-checkbox" value="Wednesday"  type="checkbox"></input>
-
-        <label htmlFor="thursday-checkbox">Thursday</label>
-        <input id="thursday" name="thursday-checkbox" value="Thursday"  type="checkbox"></input>
-
-        <label htmlFor="friday-checkbox">Friday</label>
-        <input id="friday" name="friday-checkbox" value="Friday"  type="checkbox"></input>
-
-        <label htmlFor="saturday-checkbox">Saturday</label>
-        <input id="saturday" name="saturday-checkbox" value="Saturday"  type="checkbox"></input>
-
-        <label htmlFor="sunday-checkbox">Sunday</label>
-        <input id="sunday" name="sunday-checkbox" value="Sunday"  type="checkbox"></input>
-
-        <input type="submit" value="Submit"></input>
-      </form>
+      // <form className="form">
+        <select
+          id="number-of-days"
+          name="number-of-days"
+          placeholder="#"
+        >
+          <option value="">#</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+        </select>
+        // </form>
     )
   }
 }
