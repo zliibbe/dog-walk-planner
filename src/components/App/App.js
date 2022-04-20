7d56f82e7dc32ff88660c72dc9b19108638aef47
Zach Liibbe <zliibbe@gmail.com>
2022-04-20 08:49:32 -0600

    Add basic form and update README

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,21 @@
+import React from 'react'
 import '../App/App.css';
 import Form from '../Form/Form'
 
-function App() {
-  return (
-    <div className="App">
-      <Form></Form>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    
+  }
+
+
+  render () {
+      return (
+        <div className="App">
+          <Form></Form>
+        </div>
+      );
+  }
 }
 
 export default App;
