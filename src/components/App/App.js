c77476b1772e4fea7a2dbac7a25e529f2cc1a4c3
Zach Liibbe <zliibbe@gmail.com>
2022-04-19 23:00:48 -0600

    Fix imports

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import '../App/App.css';
+import Form from '../Form/Form'
 
 function App() {
   return (
     <div className="App">
-      
+      <Form></Form>
     </div>
   );
 }
