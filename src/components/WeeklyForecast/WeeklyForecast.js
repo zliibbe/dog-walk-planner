35efe74f751d4bbd7ee2d6c03ba985f3966ed730
Zach Liibbe <zliibbe@gmail.com>
2022-04-21 17:35:55 -0600

    Add props destructuring and pass method to Day

--- src/components/WeeklyForecast/WeeklyForecast.js
+++ src/components/WeeklyForecast/WeeklyForecast.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Day from '../Day/Day'
 import './WeeklyForecast.css'
 
-const WeeklyForecast = (props) => {
-  console.log("WF-Props: ", props)
-const weatherDays= props.forecast.map (day =>{
+const WeeklyForecast = ({ forecast, selectedDays, selectDay }) => {
+ 
+const weatherDays= forecast.map (day =>{
+  
   return <Day 
             date={day.date}
             imageSrc={day.day.condition.icon}
@@ -13,6 +14,7 @@ const weatherDays= props.forecast.map (day =>{
             minTemp={day.day.mintemp_f}
             avgTemp={day.day.avgtemp_f}
             key={day.date}
+            selectDay={selectDay}
          />
 })
 
