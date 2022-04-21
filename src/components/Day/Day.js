6516da5d9f7b51c9972e4d657bd26d8c48f26918
Zach Liibbe <zliibbe@gmail.com>
2022-04-21 17:29:51 -0600

    Add onClick method to move clicked dayto selectedDays in state

--- src/components/Day/Day.js
+++ src/components/Day/Day.js
@@ -2,11 +2,11 @@ import React from "react"
 import './Day.css'
 var dayjs = require('dayjs')
 
-const Day = ({date, imageSrc, iconText, minTemp, maxTemp, avgTemp}) => {
+const Day = ({date, imageSrc, iconText, minTemp, maxTemp, avgTemp, selectDay}) => {
   return (
-    <div className="day-container">
-      <h2 className="day-of-week">Monday</h2>
-      <h2 className="date">{date}</h2>
+    <div className="day-container" onClick={event => selectDay(date)}>
+      <p className="day-of-week">{dayjs(date).format('dddd')}</p>
+      <p className="date">{dayjs(date).format('MMMM D')}</p>
       <img className= "icon" src={imageSrc} alt="weather-icon"></img>
       <p className="icon-text">{iconText}</p>
       <p className="temp-high">High: {maxTemp}°</p>
