ebd30adab97c4715781fc93bfd4160378f6fdf61
Zach Liibbe <zliibbe@gmail.com>
2022-04-21 23:53:14 -0600

    Add HTML/JSX to display and map through selectedDays to display each day

--- src/components/MyWalks/MyWalks.js
+++ src/components/MyWalks/MyWalks.js
@@ -1,21 +1,37 @@
 import React from "react"
 import './MyWalks.css'
 import Day from '../Day/Day'
+import { Route, NavLink } from 'react-router-dom';
 
-const MyWalks = ({ walks }) => {
-  
-  
-  <section className="my-walks">
-    <section className="my-walks-header">
-      <h1 className="title">Dog Walk Planner</h1>
-      {/*<Home btn>*/}
-      <p className="walking-days">I'm walking # days this week.`</p>
+const MyWalks = ({ selectedDays, numberOfDays }) => {
+  console.log("selectedDays: ", selectedDays)
+  console.log("numberOfDays: ", numberOfDays);
+  const days = selectedDays.map(day => {
+    return <Day 
+            date={day.date}
+            imageSrc={day.day.condition.icon}
+            iconText={day.day.condition.text}
+            maxTemp={day.day.maxtemp_f}
+            minTemp={day.day.mintemp_f}
+            avgTemp={day.day.avgtemp_f}
+            key={day.date}
+            // selectDay={selectDay}
+         />
+  })
+  return(
+  <div className="my-walk">
+    <section className="my-walks-container">
+      <section className="my-walks-header">
+        <h1 className="title">Dog Walk Planner</h1>
+        <p className="walking-days">I'm walking {`${numberOfDays}`} days this week.</p>
+      </section>
+      <section className="my-walks-display">
+        {days}
+      </section>
+      <NavLink to="/" className="nav">Home</NavLink>
     </section>
-    <section className="my-walks-display">
-      <Day />
-    </section>
-  </section>
-
+  </div>
+  )
 }
 
 export default MyWalks
