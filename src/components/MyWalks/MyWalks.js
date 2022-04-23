32c6c71f79e9fdc83c1143ae2f967a73466003e1
Zach Liibbe <zliibbe@gmail.com>
2022-04-22 19:29:34 -0600

    Change to class-based component

--- src/components/MyWalks/MyWalks.js
+++ src/components/MyWalks/MyWalks.js
@@ -1,22 +1,21 @@
 import React from "react"
 import './MyWalks.css'
-import Day from '../Day/Day'
 import { Route, NavLink } from 'react-router-dom';
 
 const MyWalks = ({ selectedDays, numberOfDays }) => {
   console.log("selectedDays: ", selectedDays)
   console.log("numberOfDays: ", numberOfDays);
   const days = selectedDays.map(day => {
-    return <Day 
-            date={day.date}
-            imageSrc={day.day.condition.icon}
-            iconText={day.day.condition.text}
-            maxTemp={day.day.maxtemp_f}
-            minTemp={day.day.mintemp_f}
-            avgTemp={day.day.avgtemp_f}
-            key={day.date}
-            // selectDay={selectDay}
-         />
+    // return <Day 
+    //         date={day.date}
+    //         imageSrc={day.day.condition.icon}
+    //         iconText={day.day.condition.text}
+    //         maxTemp={day.day.maxtemp_f}
+    //         minTemp={day.day.mintemp_f}
+    //         avgTemp={day.day.avgtemp_f}
+    //         key={day.date}
+    //         // selectDay={selectDay}
+    //      />
   })
   return(
   <div className="my-walk">
--- src/components/WeeklyForecast/WeeklyForecast.js
+++ src/components/WeeklyForecast/WeeklyForecast.js
@@ -1,27 +1,49 @@
 import React from 'react'
-import Day from '../Day/Day'
 import './WeeklyForecast.css'
+var dayjs = require('dayjs')
 
-const WeeklyForecast = ({ forecast, selectedDays, selectDay }) => {
-const weatherDays= forecast.map (day =>{
-  
-  return <Day 
-            date={day.date}
-            imageSrc={day.day.condition.icon}
-            iconText={day.day.condition.text}
-            maxTemp={day.day.maxtemp_f}
-            minTemp={day.day.mintemp_f}
-            avgTemp={day.day.avgtemp_f}
-            key={day.date}
-            selectDay={selectDay}
-         />
-})
+class WeeklyForecast extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-  return (
-    <section className="weekly-calendar">
-      {weatherDays}
-    </section>
-  )
+  componentDidMount() {
+    this.props.forecast.forEach(day => {
+      this.setState({ [day.date]: ''})
+    })
+  }
+
+  callTwoFunctions = (event) => {
+    this.props.selectDay(event.currentTarget.id)
+    
+  }
+
+  render() {
+
+    const weatherDays = this.props.forecast.map (day =>{
+      const dayKey = day.date
+      const foundSelectedDay = this.props.selectedDays.findIndex(activeDay => activeDay.date === dayKey)
+
+      return (
+        <div className={`day-container ${(foundSelectedDay === -1) ? '' : 'active'}`} key={day.date} id={day.date} tabIndex={0} onClick={(event) => this.callTwoFunctions(event)}>
+          <p className="day-of-week">{dayjs(day.date).format('dddd')}</p>
+          <p className="date">{dayjs(day.date).format('MMMM D')}</p>
+          <img className= "icon" src={day.day.condition.icon} alt="weather-icon"></img>
+          <p className="icon-text">{day.day.condition.text}</p>
+          <p className="temp-high">High: {day.day.maxtemp_f}°</p>
+          <p className="temp-low">Low: {day.day.mintemp_f}°</p>
+          <p className="avg-temp">Avg: {day.day.avgtemp_f}°</p>
+        </div> 
+      )
+  })
+
+    return (
+      <section className="weekly-calendar">
+        {weatherDays}
+      </section>
+    )
+  }
 }
 
 export default WeeklyForecast
