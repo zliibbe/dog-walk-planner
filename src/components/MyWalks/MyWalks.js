e9111715fade1f83d6c5a5109db8448750a7c9ce
Zach Liibbe <zliibbe@gmail.com>
2022-04-24 01:39:59 -0600

    Add conditional for rendering MyWalks depending on available content selection

--- src/components/MyWalks/MyWalks.js
+++ src/components/MyWalks/MyWalks.js
@@ -1,20 +1,30 @@
 import React from "react"
 import './MyWalks.css'
-import { Route, NavLink } from 'react-router-dom';
 var dayjs = require('dayjs')
 
 const MyWalks = ({ favoriteDays }) => {
-  const days = favoriteDays.map(day => {
-      return <div className={'day-container'} key={day.date} id={day.date} tabIndex={0} onClick={(event) => this.callTwoFunctions(event)}>
-          <p className="day-of-week">{dayjs(day.date).format('dddd')}</p>
-          <p className="date">{dayjs(day.date).format('MMMM D')}</p>
-          <img className= "icon" src={day.day.condition.icon} alt="weather-icon"></img>
-          <p className="icon-text">{day.day.condition.text}</p>
-          <p className="temp-high">High: {day.day.maxtemp_f}°</p>
-          <p className="temp-low">Low: {day.day.mintemp_f}°</p>
-          <p className="avg-temp">Avg: {day.day.avgtemp_f}°</p>
-        </div> 
-  })
+  let days;  
+    if(!favoriteDays.length) {
+      days = (<h1>You have not added any days yet. Go back to the main page to add days.</h1>)
+    }
+    else {
+      days = favoriteDays.map(day => {
+        return (
+            <div className="day-container" key={day.date} id={day.date} tabIndex={0} onClick={(event) => this.callTwoFunctions(event)}>
+            <p className="day-of-week">{dayjs(day.date).format('dd').toUpperCase()}</p>
+            <p className="month">{dayjs(day.date).format('MMMM').toUpperCase()}</p>
+            <p className="date">{dayjs(day.date).format('DD')}</p>
+            <hr />  
+            <img className= "icon" src={day.day.condition.icon} alt="weather-icon"></img>
+            <p className="icon-text">{day.day.condition.text}</p>
+            <hr />  
+            <p className="temp-high">High: {Math.round(day.day.maxtemp_f)}°</p>
+            <p className="temp-low">Low: {Math.round(day.day.mintemp_f)}°</p>
+
+           </div>
+        )
+      })
+    }
   return(
       <section className="my-walks-display">
         {days}
